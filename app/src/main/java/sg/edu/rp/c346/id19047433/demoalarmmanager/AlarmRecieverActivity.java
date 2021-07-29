0674443f497747c82c4c495dac8b9808708b9d0d
Nicole koo <19047433@myrp.edu.sg>
2021-07-29 11:08:23 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19047433/demoalarmmanager/AlarmRecieverActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id19047433.demoalarmmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlarmRecieverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_reciever);
+    }
+}
